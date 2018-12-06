4c4a32b3449d1d21609ef7f0abedad959628de85
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-06 16:49:44 -0700

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,29 @@
+//question (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//output 7 -> 0 -> 8
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        
+    }
+}
+/*
+Step 1: let’s say I get interview at google/Microsoft and I get this question; 
+before starting to work on the problem I would read the problem carefully and I would ask question like 
+“should the code only work for just two numbers” “what should be the worst-case scenario”
+
+Step 2: use I: identify the problem, what is the problem asking me to do and what does it mean in this case the problem is asking to 
+sum two numbers in a node/link list and I can safely assume the two numbers do not contain zero. 
+
+D: Let’s assume I manage to understand the problem now 
+starting thinking about the solution how would the solution look like and how can I get there.
+
+E: Explore every possible solution, for example breaking the problem into smaller solution and explore every single problem,
+in this problem I can break it into smaller one by first identifying how can I reverse the nodes and added to one another.
+
+A: Anticipate Outcomes what would be the input’s if the user try different ones would my code still work.
+
+L: If the person who is interviewing is not satisfy with the way I approach the problem then I would like some criticism
+so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
+*/
