8ef91580a20ea4618204ac89e08e2c96a7f09b40
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-06 20:55:35 -0700

    Create PerfectSquare.java

---
+++ PerfectSquare.java
@@ -0,0 +1,20 @@
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+Step1: question to ask “What does it mean perfect square number” 
+“Does the numbers have to repeat to be perfect square?” “can you explain the second example” 
+“is the question asking to skipping numbers” “how many numbers do you have to add to be a perfect square” 
+
+Step 2: use I: identify the problem, what is the problem asking me to do and what does it mean. 
+in this case the problem is asking to sum three numbers in the array however I doesn’t say the numbers can repeat.
+            D: what does the problem mean? In this case the problem is asking to find the perfect square by adding n numbers 
+            E.
+            A: Explore every possible solution, for example breaking the problem into smaller solution and explore every single problem, 
+in this problem I can break it into smaller one by first identifying what is a perfect square, what do I need to solve this problem.
+            L:If the person who is interviewing is not satisfy with the way I approach the problem then I would like some criticism, so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
