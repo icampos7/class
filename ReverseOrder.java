3ba07e99cc6465a9540c1bc8d979283df9f52422
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-07 17:04:56 -0700

    Create ReverseOrder.java

---
+++ ReverseOrder.java
@@ -0,0 +1,17 @@
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+Step 1: let’s say I get interview at google/Microsoft and I get this question; before starting to work on the problem I would read the problem carefully and I would ask question like “should the code only work for just two numbers” “what should be the worst-case scenario”
+Step 2: use IDEAL: identify the problem, 
+what is the problem asking me to do and what does it mean in this case the problem is asking to sum two numbers in a node/link list and I can safely assume the two numbers do not contain zero. 
+ 	IDEAL: Let’s assume I manage to understand the problem now starting thinking
+  about the solution how would the solution look like and how can I get there.
+	IDEAL: Explore every possible solution, 
+  for example breaking the problem into smaller solution and explore every single problem, in this problem I can break it into smaller one by first identifying how can I reverse the nodes and added to one another.
+	IDEAL: Anticipate Outcomes what would be the input’s if the user try different
+  ones would my code still work.
+	IDEAL: If the person who is interviewing
+  `is not satisfy with the way I approach the problem then I would like some criticism, so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
