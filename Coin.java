921adaeb8c45edba6816c0ca263fd412ae68c332
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-07 17:06:34 -0700

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,19 @@
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+Example 1:
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+Note:
+You may assume that you have an infinite number of each kind of coin.
+Step1: “what does it mean by combination” “does numbers have to repeat to be a perfect square” “how many numbers are require to be a perfect square”
+Step 2: use IDEAL: identify the problem, what is the problem asking me to do and what does it mean. in this case the problem is asking 
+ 	IDEAL: what does the problem mean? In this case the problem is asking to find the perfect square by adding n numbers 
+	IDEAL: Explore every possible solution, 
+  for example breaking the problem into smaller solution and explore every single problem, in this problem I can break it into smaller one by first identifying what is a perfect square, what do I need to solve this problem.
+	IDEAL: Anticipate Outcomes what would be the input’s 
+  if the user try different ones would my code still work. What if the input was [1,0]? Will it work 
+	IDEAL: If the person who is 
+  interviewing is not satisfy with the way I approach the problem then I would like some criticism, so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
