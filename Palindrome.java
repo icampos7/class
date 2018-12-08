bcba8468bdf88070ed261c0da9d39bc6543d6b53
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-07 17:26:12 -0700

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+Given a string, your task is to count how many palindromic substrings in this string.
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+1.	The input string length won't exceed 1000.	
+Step 1: ask questions “how come the output is 6 if example 1 is the same as example 2”, “does the characters have to more than 2 or can it “ab” ”
+Step 2: use IDEAL: identify the problem, what is the problem asking me to do and what does it mean. in this case the problem is asking to get the longest string
+ 	IDEAL: what does the problem mean? 
+  In this case the problem is asking to find the largest length the goal is to print how long the string is.
+IDEAL: Explore every possible solution, 
+for example breaking the problem into smaller solution and explore every single problem, 
+	IDEAL: Anticipate Outcomes what
+  would be the input’s  if we try to input one string “a” or “aa”
+	IDEAL: If the person who is 
+  interviewing is not satisfy with the way I approach the problem then I would like some criticism, so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
