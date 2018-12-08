0f119ace111f2179595adc2fe0e5a0c7cf65b7db
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-08 13:26:32 -0700

    Create SortIntegers.java

---
+++ SortIntegers.java
@@ -0,0 +1,21 @@
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+Note: You are not suppose to use the library's sort function for this problem.
+Example:
+Input: [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Follow up:
+•	A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+•	Could you come up with a one-pass algorithm using only constant space?
+?
+Step 1:"what is the time complexity"
+Step 2:I :identify the problem.what is the problem asking.in this case the problem is
+asking to sort the array without using any libary in this case i would do a forloop
+for() this will read the array and the second forloop i would read again and compare each
+number if(the previous numbers is less than the next then num it would take the next).
+  D:what does the problem mean?
+  E:one of the possible solution is compare the previous number with the next one 
+  A:What will be the outcomes if there diffrent numbers or negative numbers
+  L:how can i improved 
+  
