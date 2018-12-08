5d64e02ed1f89eccd0f5b28a26866774be52cb90
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-07 17:27:24 -0700

    Create ReverseLinkedlist.java

---
+++ ReverseLinkedlist.java
@@ -0,0 +1,15 @@
+Sort a linked list in O(n log n) time using constant space complexity.
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+Step1: ask question “I’m allowed to use library”, “should I worry about negative numbers ” “I’m allowed to use recursion???”
+Step 2: use IDEAL: identify the problem, what is the problem asking me to do and what does it mean. in this case the problem is asking to get the longest string
+ 	IDEAL: what does the problem mean? And how can I reach my goal??   
+IDEAL: Explore every possible solution, 
+	IDEAL: Anticipate Outcomes 
+	IDEAL: If the person who is interviewing is not satisfy with the way I approach the problem then I would like some criticism, so I can improve my strategy and the next time I apply for a job I can use this as temple it to improve.
+
