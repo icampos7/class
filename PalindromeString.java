94e3a1d0754c176964985c97d3c6ab90eb126632
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-10 08:32:01 -0700

    Rename Palindrome.java to PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,16 @@
+Given a string, your task is to count how many palindromic substrings in this string.
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+1.	The input string length won't exceed 1000.	
+step 1 Ask questions : "why is the out put 6"
+Step 2 IDEAL:what's the problem asking in this case the probem is asking to count the palindrome
+One of the possible solution is try to understand why the output is 6.
+the goal of this problem is to break it onto smaller problems which we can solve one by one
