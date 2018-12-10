f85c2b619eed6d4d161e1f1b5e45fef9626fcd70
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-10 11:38:19 -0700

    Update PalindromicSubProblem2.java

--- PalindromicSubProblem2.java
+++ PalindromicSubProblem2.java
@@ -12,4 +12,22 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 Note:
 1. The input string length won't exceed 1000.
  //
-step 1 Define the problem : The problem is asking to 
+step 1 Define the problem : The problem is asking to count how many palindromes are in a
+substring if any letters repeat then count them as well one of the possible solution is
+doing a counter.First we start by reading the string/substring  we will let N be the length
+of the String
+for(i = 0; i< N; i++) check how long the string is,after lets count the String 
+countLetters++ ,if(n is greater than 1000 then) return the string is to big.
+ this can be one of the solutions after this the counter can be save to another variable 
+ 
+ static int countString(String n){
+   
+   for(int i = 0; i < n.length; i++){
+     
+     if(n>1000)
+     s.o.p("to big")
+   }  
+   for(int i = 0; i<n){
+   count++;
+ return count;
+ }
