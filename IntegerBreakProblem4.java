1a70afc14d06b0b8c33c8c6445286937ff6d6a40
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-11 10:08:30 -0700

    Create IntegerBreakProblem4.java

---
+++ IntegerBreakProblem4.java
@@ -0,0 +1,16 @@
+Given a positive integer n, break it into the sum of at least two positive integers and maximize
+the product of those integers. Return the maximum product you can get.
+Example 1:
+Input: 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+Example 2:
+Input: 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+Note: You may assume that n is not less than 2 and not larger than 58.
+Step 1 Define the problem:The problem is asking to get the max product of the input 
+if the input was 4 
+the n it would be 4 = 2+2,2*2 = 4 so the max product can be 4 
+first we ask are self is this an array? or is it safe to assume it just integers.
+first get the user input 
