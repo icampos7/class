0a2444a7acb158c6804357a0cc97c547eb39ff8f
icampos7 <45663465+icampos7@users.noreply.github.com>
2018-12-14 12:32:50 -0700

    Update PerfectSquareProblem1.java

--- PerfectSquareProblem1.java
+++ PerfectSquareProblem1.java
@@ -11,7 +11,17 @@ Input: n = 13
 Output: 2
 Explanation: 13 = 4 + 9.
 
-step1, Define the problem:in this case the problem is asking to find the least number of
+  Case 1 : check the numbers for negative num if < 0 then dont count and skip num 
+  example [  -1 , 0 , 1 ,4 , 9] skip -1 && 0 check other numbers 
+  case 2: remove the negative numbers 
+  example [ -1,0,1,4,9] -> [1 ,4 , 9] check for perfect num
+  
+  case 3: replace the num with diffrent ones 
+  example [-1,0,1,4,9] -> [2,3,1,4,9]
+  
+  Step 2 : one way to add this is into and 2D array or 1D or even queues->this way we can pop and push an new value 
+  
+step1 : Define the problem:in this case the problem is asking to find the least number of
 perfect square.If the array is [1,4,9,16] if the user is inputing n then the code should
 travers to each number that adds up to n.In this case example 1 : is 12 and the numbers 
 that add up is 4 + 4 + 4 
